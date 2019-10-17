f027208e3896064ad9a6735abd36550c73198b06
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2019-10-17 15:08:58 -0500

    Work in progress

--- js/wm-animations.js
+++ js/wm-animations.js
@@ -1,8 +1,29 @@
-$(document).ready(function() {
-
-});
+$(document).ready(function() {});
 
 $(window).on('load', function() {
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {
+    //alert('scroll');
+    fixedHeader();
+  };
+
+  // Get the header
+  var $headerWrapper = $('.header');
+
+  // Get the offset position of the navbar
+  var sticky = $headerWrapper.height();
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function fixedHeader() {
+    if (window.pageYOffset > sticky) {
+      $headerWrapper.removeClass('header--top');
+      $headerWrapper.addClass('header--scroll');
+    } else {
+      $headerWrapper.addClass('header--top');
+      $headerWrapper.removeClass('header--scroll');
+    }
+  }
+
   //Jquery Slide Animation for Mobile Nav Menu
   $('.header__nav__button').click(function() {
     $('.header__nav__menu__entry').stop().slideToggle(250);
@@ -26,7 +47,7 @@ $(window).on('load', function() {
 
   function checkServicesWidth() {
     var maxServiceWidth = 0;
-    $('.wrapper--services .services .services-list li').each(function () {
+    $('.wrapper--services .services .services-list li').each(function() {
       if ($(this).width() >= maxServiceWidth) {
         maxServiceWidth = $(this).width();
       }
